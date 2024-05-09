6e79371555f1c3efacd192c03ddcf66cd074b67b
niravassar-hitrust <nirav.assar@hitrustalliance.net>
2024-05-09 18:20:37 -0500

    multi part file

--- src/main/java/com/gotsoccer/compare/controller/GotSoccerCompareController.java
+++ src/main/java/com/gotsoccer/compare/controller/GotSoccerCompareController.java
@@ -2,7 +2,10 @@ package com.gotsoccer.compare.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @AllArgsConstructor
@@ -11,5 +14,10 @@ public class GotSoccerCompareController {
     @GetMapping("/gotsoccer/compare")
     public String getAllTeams() {
         return "hello";
+    }
+
+    @PostMapping("/gotsoccer/upload")
+    public String upload(@RequestParam("file")MultipartFile multipartFile) {
+        return "file-hello";
     }
 }
