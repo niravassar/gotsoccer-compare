aa3441ba9a43f42ba3fe7552865c4ab1a711027b
niravassar-hitrust <nirav.assar@hitrustalliance.net>
2024-05-10 16:37:46 -0500

    compare bdd test

--- src/test/java/com/gotsoccer/compare/bdd/BddTest.java
+++ src/test/java/com/gotsoccer/compare/bdd/BddTest.java
@@ -2,28 +2,18 @@ package com.gotsoccer.compare.bdd;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.gotsoccer.compare.controller.CompareController;
 import com.gotsoccer.compare.domain.GameChange;
 import com.gotsoccer.compare.domain.GameValueChange;
-import com.gotsoccer.compare.service.GameService;
 import com.gotsoccer.compare.utils.MockUtils;
-import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.List;
-import java.util.Objects;
-import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
