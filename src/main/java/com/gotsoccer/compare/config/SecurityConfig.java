c136106a2bcc48a505b743e9f3950763b6fba988
nassar <assarn@objectcomputing.com>
2024-05-17 14:48:14 -0500

    configure different users

---
+++ src/main/java/com/gotsoccer/compare/config/SecurityConfig.java
@@ -0,0 +1,55 @@
+package com.gotsoccer.compare.config;
+
+import org.apache.commons.math3.util.Pair;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService users() {
+        return new InMemoryUserDetailsManager(createUsers());
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/**").authenticated())
+                .httpBasic(withDefaults())
+                .formLogin(withDefaults())
+                .build();
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    private List<UserDetails> createUsers() {
+        List<Pair<String, String>> credentials = List.of(
+                new Pair<>("nirav", "soccer123"),
+                new Pair<>("rina", "rina")
+        );
+        return credentials.stream()
+                .map(c -> User.withUsername(c.getKey()).password(passwordEncoder().encode(c.getValue())).build())
+                .toList();
+    }
+}
+
