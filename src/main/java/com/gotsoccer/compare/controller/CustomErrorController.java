b885cb8e66450ca0ddcfb99788a70f794a76baa7
nassar <assarn@objectcomputing.com>
2024-05-21 16:01:00 -0500

    custom error page

---
+++ src/main/java/com/gotsoccer/compare/controller/CustomErrorController.java
@@ -0,0 +1,18 @@
+package com.gotsoccer.compare.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request, Model model) {
+        model.addAttribute("errorMessage", request.getAttribute(RequestDispatcher.ERROR_EXCEPTION).toString());
+        return "error";
+    }
+}
